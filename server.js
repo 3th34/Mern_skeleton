69052f9dc3b0654f85fc9b34717ea3175acb512f
Xian Jay <124547140+3th34@users.noreply.github.com>
2024-06-10 11:27:17 -0400

    backend front end set up needs customization

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ import config from './client/config/config.js'
 import app from './server/express.js'
 import mongoose from 'mongoose'
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, { useNewUrlParser: true,
-useCreateIndex: true,
-useUnifiedTopology: true } )
+mongoose.connect(config.mongoUri, { 
+    //useNewUrlParser: true,
+//useCreateIndex: true,
+//useUnifiedTopology: true
+})
+//needs to show connection confirmation
+.then(() => {
+     console.log("Confirmation!, Connected");
+    })
+
 mongoose.connection.on('error', () => {
-throw new Error(`unable to connect to database: ${mongoUri}`)
+throw new Error(`unable to connect to database: ${config.mongoUri}`)//or config.mongoURI
 })
 app.get("/", (req, res) => {
 res.json({ message: "Welcome to User application." });
