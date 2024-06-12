b10bc7763069f028a681b1b46c517813111a9d48
Xian Jay <124547140+3th34@users.noreply.github.com>
2024-06-12 15:00:26 -0400

    backend front end concurrent set up

--- server/express.js
+++ server/express.js
@@ -5,13 +5,16 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from './../template.js'
-
+import userRoutes from './routes/user.routes.js'
 const app = express()
-//this is a get request to the root section templat.js to render
-app.get('/', (req,res) => {
-    res.status(200).send(Template())
+//...
+app.get('/', (req, res) => {
+res.status(200).send(Template())
 })
-
+//...
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', userRoutes)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
